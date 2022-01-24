487df4e855486b64b8638fd1742de3ab9d4b8cce
Stephanie Ly <stephanie.ly@polymtl.ca>
2022-01-23 21:29:11 -0500

    Add play solo and ranking button

---
+++ app/src/main/java/com/example/dominoapp/AddScoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.dominoapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddScoreActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_scores);
+    }
+}
---
+++ app/src/main/java/com/example/dominoapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dominoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.goToAddScoreActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddScoreActivity();
+            }
+        });
+    }
+
+    public void openAddScoreActivity(){
+        Intent intent = new Intent(this, AddScoreActivity.class);
+        startActivity(intent);
+    }
+}
